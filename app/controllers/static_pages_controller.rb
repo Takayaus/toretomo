dad01faa8b35c005823b32594bea02f6e98d0557
takayamori <morit617@gmail.com>
2020-06-23 19:48:34 +0900

    微調整

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @gyms = Gym.order(created_at: :desc).limit(4)
+    @trainers = Trainer.order(created_at: :desc).limit(10)
   end
 end
