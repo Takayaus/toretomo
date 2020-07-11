f1519856647032ed07bf50eadbf714fdd5e9dbd2
takayamori <morit617@gmail.com>
2020-07-11 17:35:19 +0900

    credentialsにkey追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AKIA5IN3I4CPNB3SZKXY'],
-      aws_secret_access_key: ENV['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'],
-      region: ENV['ap-northeast-1']
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
     }
 
-    config.fog_directory  = ENV['okuhakurai']
+    config.fog_directory  = 'okuhakurai'
     config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/okuhakurai"
     config.cache_storage = :fog
   end
