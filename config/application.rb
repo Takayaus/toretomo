c46644a406daf2e5e85747f9501f1fbf9d009a27
takayamori <morit617@gmail.com>
2020-07-13 20:50:48 +0900

    readmore,css編集

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Portfolio1
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     Rails.application.config.assets.unknown_asset_fallback = true
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
