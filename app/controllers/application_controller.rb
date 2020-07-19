b38ac614dfbd4497178d2c2dbe1a093b9de9ee93
takayamori <morit617@gmail.com>
2020-07-19 22:44:01 +0900

    sign in/redirect変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,27 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :set_search
 
+    after_action :store_location
+
+def store_location
+ # 今回の場合は、 /users/sign_in , /users/sign_up, /users/password にアクセスしたとき、ajaxでのやりとりはsessionには保存しない。
+    if (request.fullpath != "/users/sign_in" && \
+        request.fullpath != "/users/sign_up" && \
+        request.fullpath != "/users/password" && \
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+end
+
+#ログイン後のリダイレクトをログイン前のページにする場合
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+#ログアウト後のリダイレクトをログアウト前のページにする場合
+def after_sign_out_path_for(resource)
+  session[:previous_url] || root_path
+end
     def new
       @contact = Contact.new
     end
@@ -11,7 +32,7 @@ class ApplicationController < ActionController::Base
       @q = Gym.ransack(params[:q])
     end
     private
-        def sign_in_required
+    def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
     end
 
