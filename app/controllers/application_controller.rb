f3bff30345816ee568095e5edf49c6486f9981ae
takayamori <morit617@gmail.com>
2020-07-20 16:33:16 +0900

    .env追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,30 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :set_search
+      after_action :store_location
 
-    after_action :store_location
-
-def store_location
- # 今回の場合は、 /users/sign_in , /users/sign_up, /users/password にアクセスしたとき、ajaxでのやりとりはsessionには保存しない。
-    if (request.fullpath != "/users/sign_in" && \
-        request.fullpath != "/users/sign_up" && \
-        request.fullpath != "/users/password" && \
-        !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath 
+    def store_location
+        if (request.fullpath != "/users/sign_in" && \
+            request.fullpath != "/users/sign_up" && \
+            request.fullpath != "/users/password" && \
+            !request.xhr?)
+          session[:previous_url] = request.fullpath 
+        end
     end
-end
 
-#ログイン後のリダイレクトをログイン前のページにする場合
-def after_sign_in_path_for(resource)
-  session[:previous_url] || root_path
-end
+    def after_sign_in_path_for(resource)
+      case resource
+      when User
+        session[:previous_url] || root_path
+      when Admin
+        admin_root_path
+      end
+    end
 
-#ログアウト後のリダイレクトをログアウト前のページにする場合
-def after_sign_out_path_for(resource)
-  session[:previous_url] || root_path
-end
+    def after_sign_out_path_for(resource)
+      session[:previous_url] || root_path
+    end
+
     def new
       @contact = Contact.new
     end
@@ -31,6 +33,7 @@ end
     def set_search
       @q = Gym.ransack(params[:q])
     end
+    
     private
     def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
