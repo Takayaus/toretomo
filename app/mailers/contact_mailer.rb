daa0538e0fde4aca4145719d60888bb2381b29c5
takayamori <morit617@gmail.com>
2020-07-26 19:18:46 +0900

    contact_form追加

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,17 @@
 class ContactMailer < ApplicationMailer
 
+  # def contact_mail(contact)
+  #   @contact = contact  
+  #   mail to: ENV['MAIL'], subject: "メールのタイトル"
+  # end
+
   def contact_mail(contact)
-    @contact = contact  
-    mail to: ENV['MAIL'], subject: "メールのタイトル"
+    @contact = contact
+    mail(
+      from: 'sam@example.com',
+      to:   'morit617@gmail.com',
+      subject: 'お問い合わせ通知'
+    )
   end
+
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -2,8 +2,10 @@
 class ContactMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/contact_mailer/contact_mail
-  def contact_mail
-    ContactMailer.contact_mail
-  end
+  def contact
+     contact = Contact.new(message: "問い合わせメッセージ", message: "おはよう")
+
+     ContactMailer.contact_mail(contact)
+   end
 
 end
