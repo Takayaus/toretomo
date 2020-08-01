8323ce24a37a10c5e48bfbcfb20210b56f9a67dc
takayamori <morit617@gmail.com>
2020-08-01 19:17:16 +0900

    contacts設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,17 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "portfolio1_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'portfolio1111.herokuapp.com' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-    }
+  # config.action_mailer.default_url_options = { host: 'portfolio1111.herokuapp.com' }
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: "heroku.com",
+  #   address: "smtp.sendgrid.net",
+  #   port: 587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  #   }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
